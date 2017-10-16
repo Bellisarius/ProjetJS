7fb6c2b5b1435c748ee86d097b57be105b51697e
Alan UBEDA <alanubetton@hotmail.fr>
2017-10-16 12:00:30 +0200

    Ajout de la fonction d'ajout de vidéo par lien

--- public_html/script.js
+++ public_html/script.js
@@ -1,9 +1,12 @@
 var play = document.getElementById("play");
-var video = document.getElementById("Video1");
+var video = document.getElementById("Video");
+var sourceVideo = document.getElementById("sourceVideo");
 var skipBack = document.getElementById("skipBack");
 var skipForward = document.getElementById("skip");
 var restart = document.getElementById("restart");
-  
+var addVideo = document.getElementById("add");
+var URL = document.getElementById("URL");  
+
 play.addEventListener("click",function() {
        
        if (video.paused) {
@@ -25,4 +28,14 @@ play.addEventListener("click",function() {
     
     skipForward.addEventListener("click",function() {
         video.currentTime +=10;
+    });
+    
+    addVideo.addEventListener("click",function() {
+        if (URL.value !== ""){
+                       video.src = URL.value; //https://az813057.vo.msecnd.net/testdrive/ieblog/2011/nov/pp4_blog_demo.mp4
+                       video.load();  
+                       play.click();  
+                     } else {
+                        errMessage("Enter a valid video URL");
+                     }
     });
