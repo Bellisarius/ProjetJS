ab7d85803dd422c70dcac2fa014ab3ee863025a8
Alan UBEDA <alanubetton@hotmail.fr>
2017-10-20 16:33:22 +0200

    Création d'un liste lors de l'ajout de vidéo

--- public_html/script.js
+++ public_html/script.js
@@ -6,7 +6,9 @@ var skipForward = document.getElementById("skip");
 var restart = document.getElementById("restart");
 var addVideo = document.getElementById("add");
 var URL = document.getElementById("URL");  
-
+var listeURL = [];
+var nbrURL=0;
+var videoCheck=false;
 play.addEventListener("click",function() {
        
        if (video.paused) {
@@ -30,7 +32,7 @@ play.addEventListener("click",function() {
         video.currentTime +=10;
     });
     
-    addVideo.addEventListener("click",function() {
+    /*addVideo.addEventListener("click",function() {
         if (URL.value !== ""){
                        video.src = URL.value; //https://az813057.vo.msecnd.net/testdrive/ieblog/2011/nov/pp4_blog_demo.mp4
                        video.load();  
@@ -38,4 +40,55 @@ play.addEventListener("click",function() {
                      } else {
                         errMessage("Enter a valid video URL");
                      }
+    });*/
+    
+    addVideo.addEventListener("click",function() {
+        if(videoCheck===false)
+        {
+            if (URL.value !== "")
+            {
+                video.src = URL.value; //https://az813057.vo.msecnd.net/testdrive/ieblog/2011/nov/pp4_blog_demo.mp4
+                video.load();  
+                play.click();  
+                videoCheck = true;
+                URL.value ="";
+            } else {
+                errMessage("Enter a valid video URL");
+            }
+        }
+        else
+        {
+            if (URL.value !== "")
+            {
+                listeURL[nbrURL] = URL.value;
+                nbrURL++;
+            } else {
+                errMessage("Enter a valid video URL");
+            }
+        }
+        
+
     });
+    
+    video.addEventListener("ended", function()
+    {
+        if (nbrURL > 0)
+        {
+            video.src = listeURL[0];
+            video.load();  
+            play.click();  
+            
+            for(var i=0;i<nbrURL;i++)
+            {
+                if (i !== nbrURL-1)
+                {
+                    listeURL[i] = listeURL[i+1];
+                }
+                
+            }
+            nbrURL--;
+        }
+    });
+    
+
+    
